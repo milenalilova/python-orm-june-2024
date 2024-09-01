824016e6210ca82aa1e4d49b3673456556ef41d0
milenalilova <milenalilova78@gmail.com>
2024-09-01 17:08:17 -0300

    Added solutions

---
+++ 09.advanced_queries_in_django_exercise/caller.py
@@ -0,0 +1,13 @@
+import os
+import django
+from django.template.defaultfilters import date
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+
+# Run and print your queries
+# Run the custom manager methods
